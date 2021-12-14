a7d888f1cd94c9753d1d8b54d2866edf74dd703e
Victor M. Arzolla <victor.arzolla@gmail.com>
2021-12-13 23:02:42 -0300

    Deletado display_lines errado

--- image_processing.py
+++ image_processing.py
@@ -63,15 +63,6 @@ def draw_vanishing_point(img):
 
 
 
-def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
-    line_image = np.zeros_like(frame)
-    if lines is not None:
-        for line in lines:
-            for x1, y1, x2, y2 in line:
-                cv2.line(line_image, (x1, y1), (x2, y2), line_color, line_width)
-    line_image = cv2.addWeighted(frame, 0.8, line_image, 1, 1)
-    return line_image
-
 
 def get_mask(image):
     roadline_color = (50, 234, 157) # in bgr
@@ -112,6 +103,7 @@ def hough_transform(image):
     line_segments =cv2.HoughLines(image, rho, angle, min_threshold, None, 0, 0)
 
     return line_segments
+
 
 def display_lines(frame, lines, line_color=(0, 255, 0), line_width=2):
     line_image = np.zeros_like(frame)
