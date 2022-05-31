b67f028647d84ffcb03957e3c5ee9654300b7efe
Victor M. Arzolla <victor.arzolla@gmail.com>
2022-05-31 13:18:35 -0300

    Novo ponto de spawn, curva para esquerda

--- multiplos_sensores.py
+++ multiplos_sensores.py
@@ -102,7 +102,7 @@ def run_simulation(args, client):
         #ponto_spawn = random.choice(world.get_map().get_spawn_points())
         ponto_spawn = carla.Transform(carla.Location(x=388.704559, y=-141.879608, z=0.01), carla.Rotation(pitch=-0.005649, yaw=90.611755, roll=0.004866)) # proximo da curva, faixa melhor
         ponto_spawn = carla.Transform(carla.Location(x=-325.457489, y=12.516907, z=0.3), carla.Rotation(pitch=-0.763000, yaw=-179.927246, roll=0.002572)) # proximo da intersecção dificil
-        
+        ponto_spawn = carla.Transform(carla.Location(x=-397.941681, y=12.788073, z=0.1), carla.Rotation(pitch=-0.007445, yaw=179.632889, roll=0.005279)) # apos intersecção
         print("Spawn do carro: ",ponto_spawn)
        
 
