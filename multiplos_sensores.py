f891b1c37202988e0cefe26518f787bbd09afaae
Victor M. Arzolla <victor.arzolla@gmail.com>
2022-06-13 17:47:38 -0300

    Vibra um pouco, mas funciona

--- multiplos_sensores.py
+++ multiplos_sensores.py
@@ -142,10 +142,10 @@ def run_simulation(args, client):
         # K_psi = 0.12 parece replicar angulo do erro para steering direto da roda
 
         velocidade = 15
-        wn = 0.2625/velocidade
-        control = Controller(K_psi=0.12, K_dx=0.35)
+        wn = 0.5625/velocidade
+        control = Controller(K_psi=0.12, K_dx=0.3)
         #control = Controller(K_theta=0, K_dx=0, K_arctan=0)
-        control.setFilter(n=1, wn=wn)
+        control.setFilter(n=1, wn=0.035)
         control.setSampleTime(0.03)
         #control.setOutputLimit(0.5, -0.5)
 
