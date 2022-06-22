badac507f2b55108b5940841c8e51c2e5fc48951
Victor M. Arzolla <victor.arzolla@gmail.com>
2022-06-22 19:53:34 -0300

    Melhorando config

--- 01_config_TCC.bat
+++ 01_config_TCC.bat
@@ -1,1 +1,15 @@
-python D:\CARLA_0.9.12_win\PythonAPI\util\config.py -m Town04
+@echo off
+
+echo Weathers disponiveis:
+echo/
+ echo   ClearNight, ClearNoon, ClearSunset, CloudyNight, CloudyNoon,
+ echo   CloudySunset, Default, HardRainNight, HardRainNoon,
+ echo   HardRainSunset, MidRainSunset, MidRainyNight, MidRainyNoon,
+ echo   SoftRainNight, SoftRainNoon, SoftRainSunset, WetCloudyNight,
+ echo   WetCloudyNoon, WetCloudySunset, WetNight, WetNoon, WetSunset.
+ echo/
+set /p weather="Selecione o weather: "
+
+
+python .\config.py -m Town04 --weather weather%
+echo
