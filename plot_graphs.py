0d783e2d70154c02a7ebf6314b7761ca8d1a090d
Victor M. Arzolla <victor.arzolla@gmail.com>
2022-06-23 03:38:13 -0300

    configurando plot_graph

--- plot_graphs.py
+++ plot_graphs.py
@@ -21,18 +21,22 @@ if __name__ == '__main__':
 
 
     # Trajeto
-    plt.figure('Trajeto')
+    plt.figure('Trajeto', frameon=False)
     plt.plot(map_x, map_y, 'b-', label='Simulador')
-    plt.plot(vel15_x, vel15_y, 'r--', label='Controle, v = 18')
-    plt.legend(loc='best')
+    #plt.plot(vel15_x, vel15_y, 'r--', label='Controle, v = 18')
     
     #plt.scatter(vel15_x[0], np.negative(vel15_y[0]), s = 100, marker = '*' , color = 'green', label='Inicio', zorder=3)
-    plt.arrow(map_x[0], map_y[0], 0, -1, 
-         shape='full', color='green', length_includes_head=True, 
-         zorder=0, head_length=40., head_width=20, label='Inicio')
+    #plt.arrow(map_x[0], map_y[0], 0, -1, shape='full', color='green', length_includes_head=True, zorder=0, head_length=40., head_width=20, label='Inicio')
+    plt.xlabel('x [m]')
+    plt.ylabel('y [m]')
 
+    plt.grid()
+    plt.axis('square')
+    #plt.legend(loc='best')
+    import matplotlib.ticker as mticker
+    #plt.gca().yaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f s'))
 
-    plt.legend(loc='best')
+    plt.savefig('line_plot.pdf', bbox_inches='tight')  
     plt.show()
 
 
